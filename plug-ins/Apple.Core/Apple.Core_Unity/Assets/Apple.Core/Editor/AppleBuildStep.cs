53e01180afd62289671601408e2d0e18071f8caf
DodgyTim <theboss@dodgy.ltd>
2022-12-06 07:20:46 +1300

    Fixed support for Unity Windows & Linux editors

--- plug-ins/Apple.Core/Apple.Core_Unity/Assets/Apple.Core/Editor/AppleBuildStep.cs
+++ plug-ins/Apple.Core/Apple.Core_Unity/Assets/Apple.Core/Editor/AppleBuildStep.cs
@@ -30,6 +30,7 @@ namespace Apple.Core
             return appleBuildStepTypes;
         }
 
+#if UNITY_EDITOR_OSX
         /// <summary>
         /// Called at the beginning of performing build post process. This is invoked first for all steps.
         /// </summary>
@@ -80,5 +81,6 @@ namespace Apple.Core
         /// <param name="buildTarget"></param>
         /// <param name="pathToBuiltProject"></param>
         public virtual void OnFinalizePostProcess(AppleBuildProfile appleBuildProfile, BuildTarget buildTarget, string pathToBuiltProject) { }
+#endif
     }
 }
