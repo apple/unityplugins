cda31a2151d77ee798a37c0ac8727038f826ffeb
makeplayhappy <smile@makeplayhappy.com>
2023-04-14 11:08:22 +0100

    Fix AssetDatabase.FindAssets frameworks

--- plug-ins/Apple.Core/Apple.Core_Unity/Assets/Apple.Core/Editor/AppleFrameworkUtility.cs
+++ plug-ins/Apple.Core/Apple.Core_Unity/Assets/Apple.Core/Editor/AppleFrameworkUtility.cs
@@ -43,6 +43,23 @@ namespace Apple.Core
                 }
             }
 
+            // try without the .framework, Unity.2022 AssetDatabase.FindAssets fails with ".frameworks" 
+            if( libraryName.EndsWith(".framework") )
+            {
+                string libraryNameWithoutFramework = libraryName.Substring( 0, libraryName.LastIndexOf(".framework") );
+                results = AssetDatabase.FindAssets(libraryNameWithoutFramework);
+                foreach (string currGUID in results)
+                {
+                    string libraryPath = AssetDatabase.GUIDToAssetPath(currGUID);
+                    string[] folders = libraryPath.Split('/');
+                    if (Array.IndexOf(folders, platformString) > -1)
+                    {
+                        return libraryPath;
+                    }
+                }
+
+            }
+
             return string.Empty;
         }
 
