7227793d53ac3a18da1db32e0e099bc2dc3776b1
Adam Szofran <aszofran@apple.com>
2024-12-10 10:29:33 -0800

    Add B&I build number suffix (e.g. .1A11) to package file names.

--- scripts/shell/repack-tgzs.sh
+++ scripts/shell/repack-tgzs.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Repackage a release build .dmg image into one .tgz archive per plug-in.
 
+# Example .dmg file name: 072-14189-A_GamePlugins_GamePlugins1A11.dmg
+#                         ^part #^     ^project^  ^build version^            
+
 script_basename=`basename "$0" .sh`
 dmg=`realpath "$1"`
 outdir=`realpath "$2"`
@@ -9,6 +12,13 @@ outdir=`realpath "$2"`
 if [[ ! -f "$dmg" ]] || [[ ! -e "$outdir" ]] ; then
     echo "$script_basename: Repackage a release build .dmg image into one .tgz archive per plug-in."
     echo "Usage: $script_basename <dmg-file> <output-folder>"
+    exit
+fi
+
+# extract build version suffix (e.g. 1A11)
+build_suffix=`echo $dmg | sed -r 's/.*GamePlugins([0-9]+[A-Z]+[0-9]+)\.dmg/\1/'`
+if [[ -z "$build_suffix" ]]; then
+    echo "dmg file name doesn't seem to have a build number suffix (e.g. 1A11). Giving up."
     exit
 fi
 
@@ -32,7 +42,7 @@ for dir in */
 do
     echo "Repacking ${dir%/}."
     echo "tar -caf $outdir/${dir%/}.tgz -C ${dir%/} package"
-    tar -caf "$outdir/${dir%/}.tgz" -C "${dir%/}" package
+    tar -caf "$outdir/${dir%/}.$build_suffix.tgz" -C "${dir%/}" package
 done
 
 popd
